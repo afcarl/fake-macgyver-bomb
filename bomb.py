1557ac67bebcb31637746c35ae215659601cebf4
Andreas Poehlmann <mail@andreaspoehlmann.de>
2012-09-28 14:59:25 +0200

    works...

--- bomb.py
+++ bomb.py
@@ -2,14 +2,16 @@ import threading
 
 import time
 import serial
+import pygame
 
 from gi.repository import Gtk, Gdk, GObject
 
 GObject.threads_init()
 Gdk.threads_init()
+pygame.init()
 
 class Clock(Gtk.EventBox):
-    def __init__(self, size=300):
+    def __init__(self, size=100):
         Gtk.EventBox.__init__(self)
 
         self._size = size
@@ -47,7 +49,8 @@ class SerialThread(threading.Thread):
         self._ser.close()
 
 class Bomb:
-    def __init__(self, port='/dev/ttyACM0'):
+    def __init__(self, port='/dev/ttyACM0', adc_min=0, adc_max=1024, 
+                        time_start="23:59:59", time_steps=66):
         self._w = Gtk.Window()
         self._w.connect("delete-event", self.quit)
         self._clock = Clock()
@@ -55,8 +58,31 @@ class Bomb:
         self._thread = SerialThread(port, self._got_data)
         self._thread.start()
 
+        self._start = time.mktime(time.strptime(time_start,"%H:%M:%S"))
+        self._boom = time.mktime(time.strptime("00:00:00", "%H:%M:%S"))
+        self._steps = int(time_steps)
+        self._tstep = int(self._start - self._boom)/self._steps
+        self._RMIN = int(adc_min)
+        self._RMAX = int(adc_max)
+        self._oldval = 0
+
+    def _scale_val(self, val):
+        N = self._steps
+        X0 = self._RMIN
+        X1 = self._RMAX
+        val = min(max(val, X0), X1)
+        return int(1.*N*(val - X0)/(X1 - X0))
+
     def _got_data(self, val):
-        timestr = "00:00:%d" % val #strftime
+        #timestr = "00:00:%d" % val #strftime
+        v = self._scale_val(val)
+        if self._oldval != v:
+            self._oldval = v
+            pygame.mixer.Sound('tos-redalert.wav').play()
+
+        t = time.localtime(max( (self._start - v * self._tstep
+                                 - time.time()%self._tstep ), self._boom))
+        timestr = time.strftime("%H:%M:%S", t)
         GObject.idle_add(self._clock.set_time, timestr)
 
     def quit(self, *args):
